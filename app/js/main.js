03e456b620ed6ce3627707c546ad636c3d49a514
peschernuy <peschernuy@gmail.com>
2021-02-09 21:49:20 +0200

    added parasite, fito, aldenbazol sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,18 @@
 $(function () {
 
+  $('.parasite__slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows:false,
+    dots: true
+  })
+
+  $('.aldenbazol__slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true
+    })
 
  
 
