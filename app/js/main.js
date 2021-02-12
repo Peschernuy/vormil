1d427b584218b33abbe3a2c554578060dcd6c4c9
peschernuy <peschernuy@gmail.com>
2021-02-12 21:01:30 +0200

    add question section

--- app/js/main.js
+++ app/js/main.js
@@ -28,6 +28,32 @@ $(function () {
     dots: true
   })
 
- 
+ var accordion = function () {
+   var data = $('.accordion').attr('data-accordion');
+
+   $('.accordion-header').on('click', function (){
+     if (data === 'close'){
+       $('.accordion-body').slideUp();
+       if($(this).hasClass('active')){
+         $(this).toggleClass('active');
+       }
+       else{
+         $('.accordion-header').removeClass('active');
+         $(this).toggleClass('active');
+       }
+     }
+     else{
+       $(this).toggleClass('active');
+     }
+     $(this).next('.accordion-body').not(':animated').slideToggle();
+   });
+ }
+ accordion();
+
+
+
+  $('.accordion-header').on('.click', function() {
+    $('.accordion-header').toggleClass('accordion-header--active');
+  });
 
 });
