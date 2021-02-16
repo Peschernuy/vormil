b7c3b0d82b4161826dc3e2378203479b4c06a330
peschernuy <peschernuy@gmail.com>
2021-02-16 14:48:02 +0200

    add header and parasite section adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -67,4 +67,10 @@ $(function () {
     $('.accordion-header').toggleClass('accordion-header--active');
   });
 
+$('.menu__btn').on('click', function(){
+$('.menu__list').toggleClass('menu__list--active');
+});
+
+
+
 });
